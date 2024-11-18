74a3840df5208a0bbc848918b65622d7bee4b563
Sean Davran <seandavran@10-248-45-160.wireless.oregonstate.edu>
2024-11-17 22:54:11 -0800

    initial commit

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create a virtual environment named 'venv'
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the required packages from requirements.txt
+pip install -r requirements.txt
